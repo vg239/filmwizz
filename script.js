1ac0321874b245fa771107ab1146aa1377c7314e
Vatsal Gandhi <139644618+vg239@users.noreply.github.com>
2024-05-03 16:35:40 +0530

    Update script.js
    Added the drop down filter in order to filter out movies according to their genres

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 const key = '992dba73';
 
-
-
 var searchInput = document.getElementById('searched-item');
 
-searchInput.addEventListener('input', () => {
-    //console.log(searchInput.value);
-    getMovieInfo((searchInput.value).trim());
 
+
+searchInput.addEventListener('input', () => {
+    let genreFilter = document.getElementById('genre-filter').value;
+    console.log(genreFilter);
+    getMovieInfo((searchInput.value).trim(), genreFilter);
 });
 
-async function getMovieInfo(searchedMovie){ 
+
+var genreFilterDropdown = document.getElementById('genre-filter');
+
+genreFilterDropdown.addEventListener('change', () => {
+    let genreFilter = genreFilterDropdown.value;
+    console.log(genreFilter);
+    getMovieInfo((searchInput.value).trim(), genreFilter);
+});
+
+
+
+async function getMovieInfo(searchedMovie, genreFilter){ 
 
     const URL = `https://omdbapi.com/?s=${searchedMovie}&page=1&apikey=${key}`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
 
-    //console.log(data.Search);
     if(data.Response == "True") 
     {
-        displaySmallMovie(data.Search);
+        if(genreFilter == "All"){
+            displaySmallMovie(data.Search);
+        }
+        else{
+            displaySmallMoviewithGenre(data.Search, genreFilter);
+        }
+        
     }
 }
 
@@ -50,6 +66,45 @@ async function displaySmallMovie(allMovies){
     } 
     document.querySelector('.movie-brief').innerHTML = content;
 
+}
+
+
+async function displaySmallMoviewithGenre(allMovies,genreFilter){
+    
+    var content = '';
+
+    for(let i = 0; i < allMovies.length; i++)
+    {
+        if(allMovies[i].Poster == "N/A"){
+            allMovies[i].Poster = "./no-poster-found-img.png";
+        }
+        id = allMovies[i].imdbID;
+
+        const URL = `https://omdbapi.com/?i=${id}&apikey=${key}`;
+        const res = await fetch(URL);
+        const data = await res.json();
+
+
+        // console.log(data.Genre.split(','));
+        // console.log(genreFilter);
+
+        if((data.Genre.split(',')).map(s => s.trim()).includes(genreFilter)){
+            console.log("Genre Matched");
+
+            content += `
+            <a href = "maindetails.html?id=${id}">
+            <div class = "brief-info">
+                <img src = ${(allMovies[i]).Poster} alt = "${(allMovies[i]).Title}"> 
+                <div class = "brief-movie-title">
+                    <h1 class = "brief-info-title">${(allMovies[i]).Title}</h1>
+                </div>
+                <h3 class = "brief-info-h3">${(allMovies[i]).Year}</h3>
+            </div>
+            </a>`;
+        }
+    }
+
+    document.querySelector('.movie-brief').innerHTML = content;
 }
 
 
@@ -98,12 +153,3 @@ async function getMovieDetails(){
         document.querySelector('.mainMovieDetails').innerHTML=content;
         
 }
-
-
-
-
-
-
-
-
-
